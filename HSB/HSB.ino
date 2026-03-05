c33304f16a4aa017ae9c8f22215a499b20d825df
Hrivu <hrivuprasunhalder@gmail.com>
2026-03-05 18:33:07 +0600

    twaek

--- HSB/HSB.ino
+++ HSB/HSB.ino
@@ -9,7 +9,7 @@ int mtr_B_in2= 9;
 int ch1= 2;
 int ch3= 3;
 
-void setup() {
+void setup() {  
 
 
   pinMode(mtr_A_en,OUTPUT);
@@ -33,7 +33,7 @@ void loop() {
   int throttle= pulseIn(ch3,HIGH,25000);
 
 
-  int speed= map(throttle,1000,2000,25,255);
+  int speed= map(throttle,1000,2000,40,255);
   analogWrite(mtr_A_en,speed);
   analogWrite(mtr_B_en,speed);
   
@@ -43,13 +43,13 @@ void loop() {
   digitalWrite(mtr_B_in1,LOW);
   digitalWrite(mtr_B_in2,HIGH);
 
-  if(steer>1600){
+  if(steer>1650){
      
      analogWrite(mtr_A_en,(speed-100));
     
   }
 
-  else if(steer< 1400)
+  else if(steer< 1350)
   {
     analogWrite(mtr_B_en,(speed-100));
   }
